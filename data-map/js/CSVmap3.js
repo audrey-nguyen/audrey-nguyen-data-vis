302043d73c4e4d2899aa5ad9b50196e32669f406
Audrey <audreytnguy@gmail.com>
2021-04-08 14:42:39 -0500

    4/8

--- data-map/js/CSVmap3.js
+++ data-map/js/CSVmap3.js
@@ -1,63 +1,58 @@
 let myMap;
 let canvas;
 let countries;
+let locationImage;
+let imageArray = [];
+let commissionValue = [];
 
+//array for objects
+let locationImageArray = [];
 
 function preload() {
   // Load the data
-  countries = loadTable('assets/Locations.csv', 'csv', 'header');
+  countries = loadTable('assets/Locations.csv', 'csv', 'header', commissionValue,  imageLoader);
 }
 
+function imageLoader(){
+  for (let i = 0; i < countries.getRowCount(); i++){
+      imageArray[i] = loadImage("assets/images/" + countries.getString(i, 'image'));
+  }
+}
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // createCanvas(windowWidth, windowHeight);
   var map = L.map('mapid').setView([0, 0], 2);
 
  	L.tileLayer('https://api.maptiler.com/maps/voyager/{z}/{x}/{y}.png?key=6e5dj7C0TZ5PY4pS6DXo', {
  }).addTo(map);
 
-  // L.marker([41.911132813, -87.680976868]).addTo(map)
-  //    .bindPopup('Home')
-  //    .openPopup();
-
-     //parse through the table
-  //    for(let i = 0; i < countries.getRowCount(); i++ ){
-  //      //create long, lat, country and tradeValue variables
-  //      let long = Number(countries.getString(i, 'long'));
-  //      let lat = Number(countries.getString(i, 'lat'));
-  //      let country = String(countries.getString(i, 'country'));
-  //      let tradeValue = String(countries.getString(i, 'trade value'));
-  //
-  //      //update marker for each country
-  //       L.marker([lat,long]).addTo(map)
-  //           .bindPopup('<p>'+ country + '</p><p><b>' + tradeValue +'</b></p>')
-  //           .openPopup();
-  //
-  //       //connect the lines, always using Chicago as the first long, lat coordinate
-  //       var latlngs = [
-  //           [41.911132813, -87.680976868],
-  //           [lat,long],
-  //       ];
-  //       var polyline = L.polyline(latlngs, {color: 'white'}).addTo(map);
-  // }
-
 
   for(let i = 0; i < countries.getRowCount(); i++ ){
+    // locationImageArray.push(new LocationImage(imageArray[i]));
     //create long, lat, country and tradeValue variables
     let long = Number(countries.getString(i, 'long'));
     let lat = Number(countries.getString(i, 'lat'));
     let tea = Number(countries.getString(i, 'commission'));
     let country = String(countries.getString(i, 'country'));
     let tradeValue = String(countries.getString(i, 'trade value'));
+    let locationImage = String(countries.getString(i, 'image'));
+    // let locationImage = loadImage("assets/images/" + countries.getString(i, 'image'));
+    let commissionValue = Number(countries.getString(i, 'commission'));
 
 
 
-    //update marker for each country
+    // update marker for each country
     L.marker([lat,long]).addTo(map)
-        .bindPopup('<p><b>'+ country + '</b></p>Trade Value: ' + tradeValue +'</p>')
+        .bindPopup('<h1>'+ country + '</h1><img src= "assets/images/'+ locationImage +'"/><p>Trade Value: ' + tradeValue +'</p>')
         .openPopup();
 
+    // L.marker([lat,long]).addTo(map)
+    //         .bindPopup('<h1>'+ country + '</h1>'+ this.x +'<p>Trade Value: ' + tradeValue +'</p>')
+    //         .openPopup();
 
 
+//add curved poly lines
   var latlngs = [];
 
   var latlng1 = [41.911132813, -87.680976868],
@@ -80,24 +75,24 @@ function setup() {
   var midpointLatLng = [midpointY, midpointX];
 
   latlngs.push(latlng1, midpointLatLng, latlng2);
+  
+  //trying to change path thickness
+    var pathOptions = {
+    	color: 'rgba(255,0,0,0.5)',
+    	weight: 5,
+    }
 
-  var pathOptions = {
-  	color: 'rgba(255,0,0,0.5)',
-  	weight: 'tea',
-  }
   var curvedPath = L.curve(
   	[
   		'M', latlng1,
   		'Q', midpointLatLng,
   			 latlng2
   	], pathOptions).addTo(map);
-
-
 }
-
+//end curved poly lines
 
  }
 
 function draw() {
 
-}
+ }
---
+++ data-map/js/toggle-button.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+    $('#dismiss-popup-btn').click(function(){
+        $('.popup').toggleClass("active");
+    })
+
+    $('#open-popup-btn').click(function(){
+        $('.popup').toggleClass("active");
+     })
+
+})
